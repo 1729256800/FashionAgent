2ae8b5bd2ac488e733558bb5127c51d72d5d6ced
朱梓皓 <144251900+1729256800@users.noreply.github.com>
2025-05-03 21:18:39 +0800

    Update search_config.py

--- metagpt/configs/search_config.py
+++ metagpt/configs/search_config.py
@@ -17,8 +17,7 @@ class SearchConfig(YamlModel):
     """Config for Search"""
 
     api_type: SearchEngineType = SearchEngineType.SERPAPI_GOOGLE
-    # api_key: str = "c230866334b218a6155769df8d1428764fa71c577784460c50564347d094fb9d"
-    api_key: str = "8dc333797495c72a93a0c0520fe610bd390f90dfb5611ec8b198185b0e08df49"
+    api_key: str = 
     cse_id: str = ""  # for google
     search_func: Optional[Callable] = None
     params: dict = Field(
